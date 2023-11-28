cac08bd240f7909fd3c5c5c75e76dfab1d53eb16
AnhTaiNguyen <nducanhtai@gmail.com>
2023-11-28 13:29:20 +0700

    Submit
    Submit

--- hlsinterview/lib/Widget/customAppBar.dart
+++ hlsinterview/lib/Widget/customAppBar.dart
@@ -11,17 +11,51 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      title: SizedBox(
+        height: preferredSize.height,
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 10.0),
+          child: Image(image: AssetImage('assets/logo.png')),
+        ),
+      ),
       backgroundColor: Colors.transparent,
       elevation: 0,
       automaticallyImplyLeading: automaticallyImplyLeading,
       iconTheme: IconThemeData(color: Colors.black),
       actions: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Image(image: AssetImage('assets/logo.png')),
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    'Handcrafted by',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6!
+                        .copyWith(color: Colors.black54, fontSize: 12.0),
+                  ),
+                  Text(
+                    'Jim HLS',
+                    style: Theme.of(context)
+                        .textTheme
+                        .subtitle1!
+                        .copyWith(color: Colors.black, fontSize: 16.0),
+                  ),
+                ],
+              ),
+            ),
             IconButton(
-              icon: Icon(Icons.account_box),
+              icon: Image(
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  'assets/profile.png',
+                ),
+              ),
               onPressed: () {
                 Navigator.pushNamed(
                   context,
--- hlsinterview/lib/joker.dart
+++ hlsinterview/lib/joker.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:hlsinterview/navBar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'Widget/customAppBar.dart';
 import 'jokeButton.dart';
+import 'navBar.dart';
 
 class Joke {
   final String text;
   final String id;
+  List<String> votedJokeIds;
 
-  Joke({required this.text, required this.id});
+  Joke({required this.text, required this.id, required this.votedJokeIds});
 }
 
 class JokerPage extends StatefulWidget {
@@ -22,30 +23,63 @@ class _JokerPageState extends State<JokerPage> {
   late List<Joke> jokes;
   late int currentJokeIndex = 0;
 
+  @override
   void initState() {
     super.initState();
-    // Initialize jokes and currentJokeIndex
     jokes = [
       Joke(
-          id: '1',
-          text:
-              'A child asked his father, "How were people born?" So his father said, "Adam and Eve made babies, then their babies became adults and made babies, and so on." The child then went to his mother, asked her the same question, and she told him, "We were monkeys then we evolved to become like we are now." The child ran back to his father and said, "You lied to me!" His father replied, "No, your mom was talking about her side of the family."'),
+        id: '1',
+        text:
+            'A child asked his father, "How were people born?" So his father said, "Adam and Eve made babies, then their babies became adults and made babies, and so on." The child then went to his mother, asked her the same question, and she told him, "We were monkeys then we evolved to become like we are now." The child ran back to his father and said, "You lied to me!" His father replied, "No, your mom was talking about her side of the family."',
+        votedJokeIds: [],
+      ),
       Joke(
-          id: '2',
-          text:
-              'Teacher: "Kids,what does the chicken give you?" Student: "Meat!" Teacher: "Very good! Now what does the pig give you?" Student: "Bacon!" Teacher: "Great! And what does the fat cow give you?" Student: "Homework!"'),
+        id: '2',
+        text:
+            'Teacher: "Kids,what does the chicken give you?" Student: "Meat!" Teacher: "Very good! Now what does the pig give you?" Student: "Bacon!" Teacher: "Great! And what does the fat cow give you?" Student: "Homework!"',
+        votedJokeIds: [],
+      ),
       Joke(
-          id: '3',
-          text:
-              'The teacher asked Jimmy, "Why is your cat at school today Jimmy?" Jimmy replied crying, "Because I heard my daddy tell my mommy, "I am going to eat that pussy once Jimmy leaves for school today!"'),
+        id: '3',
+        text:
+            'The teacher asked Jimmy, "Why is your cat at school today Jimmy?" Jimmy replied crying, "Because I heard my daddy tell my mommy, "I am going to eat that pussy once Jimmy leaves for school today!"',
+        votedJokeIds: [],
+      ),
       Joke(
-          id: '4',
-          text:
-              'A housewife, an accountant and a lawyer were asked "How much is 2+2?" The housewife replies: "Four!". The accountant says: "I think it either 3 or 4. Let me run those figures through my spreadsheet one more time." The lawyer pulls the drapes, dims the lights and asks in a hushed voice, "How much do you want it to be?'),
-      // Add more jokes as needed
+        id: '4',
+        text:
+            'A housewife, an accountant and a lawyer were asked "How much is 2+2?" The housewife replies: "Four!". The accountant says: "I think it either 3 or 4. Let me run those figures through my spreadsheet one more time." The lawyer pulls the drapes, dims the lights and asks in a hushed voice, "How much do you want it to be?',
+        votedJokeIds: [],
+      ),
     ];
-    // Load user votes from preferences
     loadVotes();
+  }
+
+  Future<void> loadVotes() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    for (Joke joke in jokes) {
+      joke.votedJokeIds;
+    }
+  }
+
+  Future<void> voteForJoke(String jokeId, bool liked) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    // Get the list of voted joke IDs
+    late List<String> votedJokeIds = prefs.getStringList('votedJokeIds') ?? [];
+
+    votedJokeIds.add(jokeId);
+
+    prefs.setStringList('votedJokeIds', votedJokeIds);
+  }
+
+  void showNextJoke() {
+    setState(() {
+      if (jokes.isNotEmpty) {
+        currentJokeIndex = (currentJokeIndex + 1) % jokes.length;
+      }
+    });
   }
 
   @override
@@ -94,7 +128,7 @@ class _JokerPageState extends State<JokerPage> {
                 Container(
                   color: Colors.white,
                   padding: EdgeInsets.all(16.0),
-                  child: jokes.isNotEmpty
+                  child: jokes.isNotEmpty && currentJokeIndex < jokes.length
                       ? Text(jokes[currentJokeIndex].text)
                       : Text('No jokes available'),
                 ),
@@ -110,7 +144,6 @@ class _JokerPageState extends State<JokerPage> {
                       onPressed: () {
                         voteForJoke(jokes[currentJokeIndex].id, true);
                         showNextJoke();
-                        // Load user votes from preferences
                         loadVotes();
                       },
                     ),
@@ -119,9 +152,8 @@ class _JokerPageState extends State<JokerPage> {
                       buttonColor: Colors.green,
                       buttonText: 'This is not Funny.',
                       onPressed: () {
-                        voteForJoke(jokes[currentJokeIndex].id, false);
+                        voteForJoke(jokes[currentJokeIndex].id, true);
                         showNextJoke();
-                        // Load user votes from preferences
                         loadVotes();
                       },
                     ),
@@ -138,37 +170,5 @@ class _JokerPageState extends State<JokerPage> {
               ],
             ),
     );
-  }
-
-  void loadVotes() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    List<String> votedJokeIds =
-        prefs.getKeys().where((key) => prefs.getBool(key) ?? false).toList();
-
-    // jokes.removeWhere((joke) => votedJokeIds[0]);
-    print(votedJokeIds);
-  }
-
-  void voteForJoke(String jokeId, bool liked) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    // Get the list of voted joke IDs
-    List<String> votedJokeIds =
-        prefs.getKeys().where((key) => prefs.getBool(key) ?? false).toList();
-
-    votedJokeIds.remove(jokeId);
-
-    prefs.setBool(jokeId, true);
-    // Save the updated list of voted joke IDs to shared preferences
-    prefs.setStringList('votedJokeIds', votedJokeIds);
-  }
-
-  void showNextJoke() {
-    setState(() {
-      if (jokes.isNotEmpty) {
-        currentJokeIndex = (currentJokeIndex + 1) % jokes.length;
-      }
-    });
   }
 }
